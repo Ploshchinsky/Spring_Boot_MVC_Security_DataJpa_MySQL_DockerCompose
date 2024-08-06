37d7e96ee60c160400c9435f213249739fbd3b9b
Anton Ploshchinsky <151565633+Ploshchinsky@users.noreply.github.com>
2024-08-06 19:56:41 +0500

    feat(start): project init

---
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/SpringBootMvcSecurityDataJpaMySqlDockerComposeApplication.java
@@ -0,0 +1,13 @@
+package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootMvcSecurityDataJpaMySqlDockerComposeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootMvcSecurityDataJpaMySqlDockerComposeApplication.class, args);
+	}
+
+}
