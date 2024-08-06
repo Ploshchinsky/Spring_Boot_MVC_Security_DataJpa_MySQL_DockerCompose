a2cc00cfb1500b278ec2f922b38e4a3a7f58fb11
Anton P. <made1of1fire@gmail.com>
2024-08-06 22:27:56 +0500

    feat(service): added User Service Interface Implementation Class -> UserServiceImpl.class

---
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/service/UserServiceImpl.java
@@ -0,0 +1,63 @@
+package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.entity.User;
+import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.exception.EntityValidateException;
+import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.repository.UserRepository;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService {
+    private final UserRepository userRepository;
+
+    @Override
+    public User save(User user) {
+        return null;
+    }
+
+    @Override
+    public User findById(Long id) {
+        return null;
+    }
+
+    @Override
+    public User findByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public User updateById(Long id, Map<String, Object> updates) {
+        return null;
+    }
+
+    @Override
+    public Long deleteById(Long id) {
+        return null;
+    }
+
+    @Override
+    public void validate(User user) {
+        List<String> errors = new ArrayList<>();
+        if (!user.getUsername().matches("^[a-zA-Z\\s]+$")) {
+            errors.add("Wrong username. Use only letters [a-z&A-Z]");
+        }
+        if (!user.getPassword().matches("^[\\w]{4,12}$")) {
+            errors.add("Wrong password. " +
+                    "Don't use 'space' and special character ('!', ',', '.' etc). Length from 4 to 12");
+        }
+        if (!user.getEmail().matches("^([\\w._-]+)(@)([\\w-]+\\.)([a-zA-Z]{2,4})$")) {
+            errors.add("Wrong email.");
+        }
+
+        if (!errors.isEmpty()) {
+            throw new EntityValidateException("User Validate Exception: " + errors);
+        }
+    }
+}
