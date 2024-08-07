c60a6d050395b27c2bb0742439e361e6dc7a4069
Anton P. <made1of1fire@gmail.com>
2024-08-07 16:58:54 +0500

    feat(model): added UserDto for User entity

---
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/model/UserDto.java
@@ -0,0 +1,9 @@
+package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.model;
+
+import lombok.Data;
+
+@Data
+public class UserDto {
+    private String username;
+    private String email;
+}
