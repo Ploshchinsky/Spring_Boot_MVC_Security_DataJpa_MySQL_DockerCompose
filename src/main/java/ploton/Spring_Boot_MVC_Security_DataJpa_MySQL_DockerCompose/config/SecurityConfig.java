ff99828377363bcff09050cb09d90d88b8d2a613
Anton P. <made1of1fire@gmail.com>
2024-08-08 20:35:36 +0500

    feat(cfg): updated SecurityConfig -> test mode

--- src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
@@ -7,6 +7,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -24,7 +25,9 @@ public class SecurityConfig {
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.
                 authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/users/reg").permitAll())
+                        .anyRequest().authenticated())
+                .cors(AbstractHttpConfigurer::disable)
+                .csrf(AbstractHttpConfigurer::disable)
                 .formLogin(Customizer.withDefaults())
                 .logout(Customizer.withDefaults())
                 .build();
