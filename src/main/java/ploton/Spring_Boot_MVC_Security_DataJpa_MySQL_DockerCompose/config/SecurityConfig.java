e9f9ea737c1902386faf6e5d0960660523cb50ce
Anton P. <made1of1fire@gmail.com>
2024-08-08 20:13:08 +0500

    fix(cfg): SecurityConfig updated -> added PasswordEncoder -> BCrypPasswordEncoder

--- src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
@@ -7,12 +7,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfig {
+
+    @Bean
+    BCryptPasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
 
     @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
