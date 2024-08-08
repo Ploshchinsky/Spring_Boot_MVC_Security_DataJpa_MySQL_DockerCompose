062f4b1c15678064e4a359db6d5f059b267060bb
Anton P. <made1of1fire@gmail.com>
2024-08-08 18:35:42 +0500

    feat(entity): added entity Role

---
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/entity/Role.java
@@ -0,0 +1,14 @@
+package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "roles")
+@Data
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+}
