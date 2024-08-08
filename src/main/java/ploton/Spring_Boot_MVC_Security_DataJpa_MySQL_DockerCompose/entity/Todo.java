1b6afc7c07746b3f679725d6117710d49445b8b5
Anton P. <made1of1fire@gmail.com>
2024-08-08 18:01:14 +0500

    fix(entity): fix recursive error with connected entity User

--- src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/entity/Todo.java
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/entity/Todo.java
@@ -1,5 +1,6 @@
 package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.entity;
 
+import com.fasterxml.jackson.annotation.JsonBackReference;
 import jakarta.persistence.*;
 import lombok.Data;
 
@@ -25,6 +26,7 @@ public class Todo {
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
+    @JsonBackReference
     private User user;
 
     @PrePersist
