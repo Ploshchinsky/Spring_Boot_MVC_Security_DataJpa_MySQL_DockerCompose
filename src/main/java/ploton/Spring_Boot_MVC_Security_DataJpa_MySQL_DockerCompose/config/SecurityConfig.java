72b516fe5042175cbab5d61e61f17a3bbd7d2e72
Anton P. <made1of1fire@gmail.com>
2024-08-09 18:54:53 +0500

    fix(cfg): Updated SecurityConfig -> fixed BCrypt injection

--- src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
@@ -1,11 +1,8 @@
 package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.config;
 
-import jakarta.servlet.FilterChain;
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
@@ -15,15 +12,11 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.repository.RoleRepository;
-import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.repository.UserRepository;
 import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.service.UserService;
-import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.service.UserServiceImpl;
 
 @Configuration
 @RequiredArgsConstructor
