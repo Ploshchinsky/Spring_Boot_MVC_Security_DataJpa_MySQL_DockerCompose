db1b7d36eac637e4e263e4752b4df7c02a1be57a
Anton P. <made1of1fire@gmail.com>
2024-08-09 11:36:29 +0500

    feat(model): added new DTO -> UserRoleDto

---
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/model/UserRoleDto.java
@@ -0,0 +1,9 @@
+package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.model;
+
+import lombok.Data;
+
+@Data
+public class UserRoleDto {
+    private String username;
+    private String role;
+}
