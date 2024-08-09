73fb9b98ebc50572a3dd490f3d5f53ded7080964
Anton P. <made1of1fire@gmail.com>
2024-08-09 16:58:55 +0500

    feat(cfg): Updated SecurityConfig -> updated Auth point -> from LoginForm to /api/v1/users/auth

--- src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
+++ src/main/java/ploton/Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose/config/SecurityConfig.java
@@ -2,22 +2,30 @@ package ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.config;
 
 import jakarta.servlet.FilterChain;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpStatus;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.service.UserService;
+import ploton.Spring_Boot_MVC_Security_DataJpa_MySQL_DockerCompose.service.UserServiceImpl;
 
 @Configuration
-@EnableWebSecurity
 @RequiredArgsConstructor
+@EnableWebSecurity
 public class SecurityConfig {
+    private final UserService userService;
 
     @Bean
     BCryptPasswordEncoder passwordEncoder() {
@@ -25,21 +33,36 @@ public class SecurityConfig {
     }
 
     @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration configuration)
+            throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    DaoAuthenticationProvider daoAuthenticationProvider() {
+        DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
+        provider.setPasswordEncoder(passwordEncoder());
+        provider.setUserDetailsService(userService);
+        return provider;
+    }
+
+    @Bean
     SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         return http.
                 authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/api/v1/users/reg", "/api/v1/users/login").permitAll()
+                        .requestMatchers("/api/v1/users/auth").permitAll()
+                        .requestMatchers("/api/v1/users/reg").permitAll()
                         .requestMatchers("/api/v1/users/id/").hasAnyRole("ADMIN", "MODERATOR")
                         .requestMatchers("/api/v1/users/add-role").hasAnyRole("ADMIN", "MODERATOR")
                         .requestMatchers("/api/v1/users/profile").authenticated()
                         .anyRequest().authenticated())
                 .cors(AbstractHttpConfigurer::disable)
                 .csrf(AbstractHttpConfigurer::disable)
-//                .sessionManagement(session -> session
-//                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-//                .exceptionHandling(exceptionHandling -> exceptionHandling.
-//                        authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
-                .formLogin(Customizer.withDefaults())
+                .sessionManagement(session -> session
+                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .exceptionHandling(exceptionHandling -> exceptionHandling.
+                        authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+//                .formLogin(Customizer.withDefaults())
                 .logout(Customizer.withDefaults())
                 .build();
     }
